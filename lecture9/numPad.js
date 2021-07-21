c73d7bc3407002da778b79aa96ed9094f5d261b5
vasudev19126 <vasudev19126@iiitd.ac.in>
2021-07-21 19:25:22 +0530

    lecture 9

--- lecture9/numPad.js
+++ lecture9/numPad.js
@@ -16,5 +16,22 @@ function numPad(input, numStr, arr){
 }
 
 
+function numPadOtp(input, numStr, arr, index){
+    if(index===input.length){
+        console.log(numStr);
+        return;
+    }
+
+    let ind = parseInt(input[index]);
+
+    let str = arr[ind];
+    for(let ch of str){
+        let newNumStr = numStr+ch;
+        numPadOtp(input, newNumStr, arr, index+1);
+    }
+}
+
+
 let arr = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
-numPad("22", "", arr);
+// numPad("22", "", arr);
+numPadOtp("23", "", arr, 0);
